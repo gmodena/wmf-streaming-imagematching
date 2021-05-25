77494755e70e70ea9546cc297a7d5d041fe5f44d
gmodena <gmodena@wikimedia.org>
2021-05-25 21:43:25 +0200

    WIP: demo job that reads from the revision topic.
    Add a basic job that reads from kafka and holds
    count aggregates in a Queriable State.

--- src/main/scala/org/wikimedia/platform/imagematching/Revison.scala
+++ src/main/scala/org/wikimedia/platform/imagematching/Revison.scala
@@ -1,5 +1,29 @@
 package org.wikimedia.platform.imagematching
 
-object Revison {
+case class Performer(
+                      user_text: String,
+                      user_groups: List[String],
+                      user_is_bot: Boolean,
+                      user_id: Long,
+                      user_registration_dt: String,
+                      user_edit_count: Long
+                    )
 
-}
+case class RevisionCreate(
+                           database: String,
+                           page_id: Long,
+                           page_title: String,
+                           page_namespace: Int,
+                           rev_id: Long,
+                           rev_timestamp: String,
+                           rev_sha1: String,
+                           rev_minor_edit: Boolean,
+                           rev_len: Int,
+                           rev_content_model: String,
+                           rev_content_format: String,
+                           performer: Performer,
+                           page_is_redirect: Boolean,
+                           rev_parent_id: Long,
+                           rev_content_changed: Boolean,
+                           rev_is_revert: Boolean
+                         )
--- src/main/scala/org/wikimedia/platform/imagematching/StreamingJob.scala
+++ src/main/scala/org/wikimedia/platform/imagematching/StreamingJob.scala
@@ -1,11 +1,58 @@
 package org.wikimedia.platform.imagematching
+
+import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala._
-import scala.util.Properties
+import java.util.Properties
+import io.circe.generic.auto._
+import io.circe.parser._
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
+import org.slf4j.LoggerFactory
 
-val properties = new Properties();
-properties.setProperty("bootstrap.servers", "localhost:9092");
-properties.setProperty("group.id", "test");
 
-inputStream = env.addSource (
-  new FlinkKafkaConsumer08[String](
-    "topic", new SimpleStringSchema(), pro perties))
+case class RevisionCounter(database: String, count: Int)
+
+class StreamingJob
+
+object StreamingJob {
+  private val Log = LoggerFactory.getLogger(classOf[StreamingJob])
+  private val kafkaTopic = "eqiad.mediawiki.revision-create"
+  private val kafkaBootstrapServers = "kafka-jumbo1001.eqiad.wmnet:9092"
+  private val kafkaGroupId = "ima-revision-create-test"
+
+  def main(args: Array[String]): Unit = {
+    import org.apache.flink.streaming.api.scala._
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val properties = new Properties();
+    properties.setProperty("bootstrap.servers", kafkaBootstrapServers);
+    properties.setProperty("group.id", kafkaGroupId);
+
+    val schema = new SimpleStringSchema()
+    val consumer: FlinkKafkaConsumer[String] = new FlinkKafkaConsumer[String](kafkaTopic, schema, properties)
+
+    val revisionStream: DataStream[String] = env.addSource(consumer)
+
+    revisionStream.flatMap({message: String => {
+      decode[RevisionCreate](message).fold(
+          error => {
+            Log.error { error.toString }
+            Log.error { message }
+            None
+          },
+          revision => Option(revision)
+        )}
+      }).map((revision: RevisionCreate) => RevisionCounter(database=revision.database, count=1))
+      .keyBy(_.database) // database
+      .window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
+      .sum("count")
+      .keyBy(_.database) // database
+      .asQueryableState("revision-count-state")
+
+    env.execute("IMA echo revision")
+  }
+}
